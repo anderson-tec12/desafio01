43473f9ce76fc7bb2e75650c7ee0b34770c967ed
Andersonbarros.silva <anderson.tec12@gmail.com>
2020-08-17 23:37:08 -0300

    Entraga do projeto

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -8,26 +9,96 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+function validateProjectId(req, res, next) {
+  const { id } = req.params;
+
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: "ID is not valid" });
+  }
+
+  return next();
+}
+
+let repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repositori = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+  repositories.push(repositori);
+  return response.status(200).json(repositori);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateProjectId, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositorieIndex = repositories.findIndex(
+    (repositorie) => repositorie.id === id
+  );
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: "Repositori not found" });
+  }
+
+  repositories[repositorieIndex] = {
+    ...repositories[repositorieIndex],
+    title,
+    url,
+    techs,
+  };
+
+  console.log("DAdos", repositories[repositorieIndex]);
+
+  return response.status("200").json(repositories[repositorieIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(
+    (repositorie) => repositorie.id === id
+  );
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: "Repositori not found" });
+  }
+
+  //respositories = repositories.slice(repositorieIndex, 1);
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateProjectId, (request, response) => {
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(
+    (repositorie) => repositorie.id === id
+  );
+
+  if (repositorieIndex < 0) {
+    return res.status(400).json({ error: "Repositori not found" });
+  }
+
+  const likesNow = repositories[repositorieIndex].likes + 1;
+
+  repositories[repositorieIndex] = {
+    ...repositories[repositorieIndex],
+    likes: likesNow,
+  };
+
+  //console.log("DAdos", repositories[repositorieIndex]);
+
+  return response.status("200").json(repositories[repositorieIndex]);
 });
 
 module.exports = app;
